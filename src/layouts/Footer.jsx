4d4fa643bdd2d2d2563ce0b555b4206b3bd6a0cb
proxygirl <hilahthomas@gmail.com>
2024-05-13 22:25:30 -0500

    Update About page

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -1,6 +1,7 @@
 const Footer = (props) => <>
   <footer>
-  © angelcities 2024
+    <span>© angelcities.net 2024</span>
+    <span>v.1.1.0</span>  
   </footer>
 </>
 
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,13 +5,13 @@ const About = () => <>
   <Page title = "About">
     <div>
       <p>A project 2 years in the making, blendy is a procedurally generated puzzle game based off of the mobile game Blendoku.</p>
-      <p>Special thanks to Chloe, Drew, and Eddy's nieces.</p>
-      {/* <h3>Libraries</h3>
+      <p>Special thanks to Chloe, Drew, and Eddy's nieces for all of their help playtesting early versions of the game.</p>
+      <h3>Built With</h3>
       <ul>
-        <li><a href="https://colorjs.io">Color.js</a></li>
-        <li><a href="https://chancejs.com">Chance</a></li>
-        <li><a href="https://ajv.js.org">Ajv</a></li>
-      </ul> */}
+        <li>♥ <a href="https://colorjs.io">Color.js</a></li>
+        <li>♥ <a href="https://chancejs.com">Chance</a></li>
+        <li>♥ <a href="https://ajv.js.org">Ajv</a></li>
+      </ul>
       </div>      
     <Navigation />
   </Page>
